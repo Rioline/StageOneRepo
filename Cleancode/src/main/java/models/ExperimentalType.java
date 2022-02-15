6e23f302bf5020fbe11142ca39872f85ffd1b221
Dmitry Danchenko <91329448+Rioline@users.noreply.github.com>
2022-02-15 12:56:36 +0200

    -all

--- Cleancode/src/main/java/models/ExperimentalType.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public enum ExperimentalType {
-    HIGH_ALTITUDE, HYPERSONIC, LIFTING_BODY, VTOL
-}
--- Cleancode/src/main/java/models/MilitaryType.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public enum MilitaryType {
-    BOMBER, FIGHTER, TRANSPORT
-}
